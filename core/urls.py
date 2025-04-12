22c9e88b1ec594740620e432ae434c5871965e73
Ricardo2502 <ricardo.tixe1993@gmail.com>
2025-04-12 17:57:58 -0500

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,19 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def inicio(request):
-    return HttpResponse("""
-    <html>
-        <head><title>Judicont Contabilidad</title></head>
-        <body style='text-align:center; font-family:sans-serif; margin-top:100px;'>
-            <h1>Bienvenido a Judicont Contabilidad</h1>
-            <p>Tu sistema ya está funcionando correctamente.</p>
-        </body>
-    </html>
-    """)
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio),
+    path('contabilidad/', include('apps.contabilidad.urls')),  # 👈 esta línea es CLAVE
 ]
