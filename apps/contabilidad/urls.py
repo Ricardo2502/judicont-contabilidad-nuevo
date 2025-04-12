0643ceff0d4f4caeaf5aa8915d373c712f877969
Ricardo2502 <ricardo.tixe1993@gmail.com>
2025-04-12 18:07:34 -0500

    Update urls.py

--- apps/contabilidad/urls.py
+++ apps/contabilidad/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('cuentas/', views.cuentas, name='cuentas'),
-    path('diario/', views.diario, name='diario'),
-    path('facturas/', views.facturas, name='facturas'),
-    path('formulario104/', views.formulario104, name='formulario104'),
-    path('reportes/', views.reportes, name='reportes'),
+    # Elimina o comenta estas hasta que las implementemos
+    # path('diario/', views.diario, name='diario'),
+    # path('facturas/', views.facturas, name='facturas'),
+    # path('formulario104/', views.formulario104, name='formulario104'),
+    # path('reportes/', views.reportes, name='reportes'),
 ]
