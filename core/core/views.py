930fbbf97cf7216c0f7e7f2f472df13109c5cee7
Ricardo2502 <ricardo.tixe1993@gmail.com>
2025-04-12 18:15:02 -0500

    Delete core/core directory

--- core/core/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def inicio(request):
-    return HttpResponse("<h1>Bienvenido al sistema contable Judicont</h1><p>Ve a <a href='/contabilidad/cuentas/'>Catálogo de cuentas</a></p>")
